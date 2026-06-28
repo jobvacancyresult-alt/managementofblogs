a70c86062b1fae2e2d5ad339b5fc2b018dd79d12
jobvacancyresult <jobvacancyresult@gmail.com>
2026-06-28 16:19:49 +0530

    fix: add standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     remotePatterns: [
       {
